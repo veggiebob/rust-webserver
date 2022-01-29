a905327f9f4c58df321b50014bc9c007ecb2d718
veggiebob <veggiebobowman@gmail.com>
2022-01-29 14:32:04 -0500

    change name

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ then
   echo "Needs at least 2 arguments: <website file location> <addr:port>"
 else
   cargo build --release
-  authbind --deep ./target/release/veggiebob-website "$1" "$2"
+  authbind --deep ./target/release/simple-rust-webserver "$1" "$2"
 fi
